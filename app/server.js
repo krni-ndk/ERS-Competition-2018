472a9920c9b2d5fc96f4f31107c931751dc695f1
Niko <niko@example.com>
2019-03-14 16:35:38 +0100

    Nodejs html rendering and other 'public' resources

--- app/server.js
+++ app/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const mysql = require('mysql');
 
 const PORT = 3000;
@@ -7,8 +8,14 @@ const dbConfig = require('./database-config');
 const app = express();
 const dbCon = mysql.createConnection(dbConfig);
 
-app.listen(PORT, function(xError) {
-    if (xError) throw xError;
+app.use(express.static(path.join(__dirname + '/../public')));
+
+app.get('/', function(aReq, aRes, xErr) {
+    aRes.sendFile(path.join(__dirname + "/../public/index.html"));
+});
+
+app.listen(PORT, function(xErr) {
+    if (xErr) throw xErr;
 
     console.log(`Server started on ${dbConfig.host} port ${PORT}`);
 });
