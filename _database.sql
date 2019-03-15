a68fb7328f39de37b1e1a4403b161a2fc464a4d1
niko <niko@example.com>
2019-03-15 13:29:41 +0100

    Removed 'hum' column from database

--- _database.sql
+++ _database.sql
@@ -10,6 +10,5 @@ CREATE TABLE readings (
     PRIMARY KEY (reading_id),
     reading_id INTEGER NOT NULL AUTO_INCREMENT,
     temp NUMERIC(5, 2) NOT NULL,
-    hum NUMERIC(5, 2) NOT NULL,
     time_ms BIGINT NOT NULL -- Time in miliseconds because Date().getTime() returns elapsed miliseconds from 1970
 );
