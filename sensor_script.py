1ee1fee5531e18e2fcc15ab3e2f4ed89333f4171
Niko <niko@example.com>
2019-03-17 00:41:32 +0100

    Updated sensor_script

--- sensor_script.py
+++ sensor_script.py
@@ -18,7 +18,6 @@ def store_data(temp):
     """ Stores sensor data and time stamp into database """
     mycursor = MYDB.cursor()
     time_ms = int(round(time_.time() * 1000))
-    temp = 23.4
     value = (temp, time_ms)
 
     mycursor.execute(
@@ -26,5 +25,22 @@ def store_data(temp):
     MYDB.commit()
 
 
-store_data(50.3)
-# print(read_sensor(PATH))
+def read_sensor(sensor_path):
+    """ Reads and parsers sensor file """
+    value = "U"
+    try:
+        sensor_file = open(sensor_path, "r")
+        line = sensor_file.readline()
+        if re.match(r"([0-9a-f]{2} ){9}: crc=[0-9a-f]{2} YES", line):
+            line = sensor_file.readline()
+            match = re.match(r"([0-9a-f]{2} ){9}t=([+-]?[0-9]+)", line)
+            if match:
+                value = str(float(match.group(2)) / 1000.0)
+        sensor_file.close()
+    except (IOError) as exception:
+        print(time_.strftime("%x %X"), "Error reading", sensor_path, ": ", exception)
+    return value
+
+
+print(read_sensor(PATH))
+store_data(read_sensor(PATH))
